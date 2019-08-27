bb6950bc901d90dcca85aa931bd808a42ab27223
quanghuy99x <52957397+quanghuy99x@users.noreply.github.com>
2019-08-27 18:30:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,99 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<title>ATN SHOP</title>
-		<link rel="stylesheet" type="text/css" href="home.css">
-	</head>
+    <head>
+        <title>ATN Shop</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <style>
+            .container{
+                width: 100%;
+                margin: 0 auto;
+            }
+            .container img{
+                width: 100%;
+
+            }
+            .footer{
+                width: 100%;
+                height: 100px;
+                background-color: white;
+            }
+            .main{
+                width: 100%;
+                overflow: hidden;
+                background-color: white;
+            }
+
+            .image img{
+                width: 100%;
+            }
+            .detail{
+                width: 100%;
+                float: right;
+                text-align: center;
+            }
+            .title{
+                background-color: white;
+                font-size: 25px;
+                line-height: 70px;
+                padding-left: 5px;
+                font-weight: bold;
+                color: purple;
+            }
+            .detail{
+                padding-left: 15px;
+                box-sizing: border-box;
+            }
+            .des{
+                color: red;
+                font-size: 18px;
+                padding-left: 5px;
+                padding-top: 1px;
+                font-weight: normal;
+            }
+
+            .list{
+                width: 100%;
+                padding-top: 10px;
+            }
+            .item{
+                width: 25%;
+                float: left;
+                padding: 5px;
+                box-sizing: border-box;
+            }
+            .iimage img{
+                width: 100%;
+                height : 50%;
+            }
+            .nav{
+                width: 100%;
+                height: 50px;
+                background-color: pink;
+            }
+            .nav ul{
+                margin: 0;
+                padding: 0;
+                list-style: none;
+            }
+            .nav a{
+                color:purple;
+                font-size: 30px;
+                text-decoration: cornflowerblue;
+                line-height: 50px;
+                padding-left: 185px;
+                display: block;
+            }
+            .nav li{
+                float: left;
+            }
+            .nav a:hover{
+                color: #792323;
+            }
+            .nav li:hover{
+                background-color:#DCF4F6;
+            }
+        </style>
+    </head>
 	<body>
 		<div id="menu_top">
 			<div id="ATN">
