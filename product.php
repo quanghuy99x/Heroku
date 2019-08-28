8bf7e0cd61a4772c4892ca30b849d83a74069039
quanghuy99x <52957397+quanghuy99x@users.noreply.github.com>
2019-08-28 11:29:07 +0700

    Update product.php

--- product.php
+++ product.php
@@ -6,33 +6,18 @@
 </head>
 <body>
 	<div id="menu_top">
-			<div id="ATN">
-				<a href="home.php" title="HOME PAGE">ATN SHOP</a>
-			</div>
 			<div id="menu">
 				<ul>
 				<li><a href="home.php" title="HOME PAGE">HOME PAGE</a></li>
-				
 				<li><a href="product.php" title="PRODUCT">PRODUCT</a></li>
 				
 			</ul>
 			</div>
-			
 		</div>
 		<div id="banner">
 			<img src="image/a3.jpg" alt="DO CHOI" width="1000px" />
 		</div>
 		<div id="product">
-			<div class="category col-3">
-				<ul>
-					<li><a href="product.php" title="PRODUCT">DO CHOI VAN DONG</a></li>
-					<li><a href="product.php" title="PRODUCT">DO CHOI TRI TUE</a></li>
-					<li><a href="product.php" title="PRODUCT">PHUONG TIEN GIAO THONG</a></li>
-					<li><a href="product.php" title="PRODUCT">PHU KIEN,DO DUNG</a></li>
-					<li><a href="product.php" title="PRODUCT">NHAN VAT/CHU DE</a></li>
-					
-				</ul>
-			</div>
 			<div class="PofC col-9">
 				<div class="col-6">
 					<a href="detail.php"><img src="./image/dochoi1.jpg" width="220px" /></a>
